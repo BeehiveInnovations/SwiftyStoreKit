d25db590179199273ab9300ac319f09e2f865ebf
Fahad <fahad@2doapp.com>
2025-01-23 20:19:48 +0400

    Cleanup

--- Package.swift
+++ Package.swift
@@ -19,10 +19,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "SwiftyStoreKit",
-      dependencies: [],
-      linkerSettings: [
-        .unsafeFlags(["-Wl,-make_mergeable"])
-      ]),
+      dependencies: []),
     .testTarget(
       name: "SwiftyStoreKitTests",
       dependencies: ["SwiftyStoreKit"])
