544e58a7082ab55971b4c4065112737e55e4a24a
Fahad <fahad@2doapp.com>
2025-01-23 19:30:50 +0400

    Make mergeable

--- Package.swift
+++ Package.swift
@@ -1,27 +1,30 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyStoreKit",
-    platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("11.0"), .watchOS(.v5)],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftyStoreKit",
-            targets: ["SwiftyStoreKit"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftyStoreKit",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftyStoreKitTests",
-            dependencies: ["SwiftyStoreKit"])
-    ]
+  name: "SwiftyStoreKit",
+  platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("11.0"), .watchOS(.v9)],
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "SwiftyStoreKit",
+      targets: ["SwiftyStoreKit"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "SwiftyStoreKit",
+      dependencies: [],
+      linkerSettings: [
+        .unsafeFlags(["-Wl,-make_mergeable"])
+      ]),
+    .testTarget(
+      name: "SwiftyStoreKitTests",
+      dependencies: ["SwiftyStoreKit"])
+  ]
 )
